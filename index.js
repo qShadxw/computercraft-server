14f48976c0b7b2d2d381204d9bb839b4f88c5ddb
qShadxw <tyl@null.net>
2024-09-16 14:52:37 +0100

    Added JSON Files for Authentication

--- index.js
+++ index.js
@@ -1,38 +1,56 @@
+const fs = require("fs")
 const http = require('http')
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 const computerId = "1a01ed6f-ccf4-437f-aa93-5cfe98da3170";
-const playerAuthentication = {
-    "Carbonate": 10
-}
-const doorAuthentication = {
-    "Test": 10
-}
-
+let playerAuthentication = {};
+let doorAuthentication = {};
 const server = http.createServer((req, res) => {})
 
-server.listen(8080, () => {
+function LoadJson()
+{
+    playerAuthentication = JSON.parse(fs.readFileSync(__dirname + "/authentications/players.json", 'utf8'))
+    doorAuthentication = JSON.parse(fs.readFileSync(__dirname + "/authentications/doors.json", 'utf8'))
+
+    console.log("Reloaded Json Files!")
+}
+
+server.listen(8080, () =>
+{
+    LoadJson();
     const { address, port } = server.address()
     console.log(`Server is listening on: http://${address}:${port}`)
 })
 
-server.on("request", (req, res) => {
+server.on("request", (req, res) =>
+{
     console.log(req.headers)
 
-    if (req.headers["user-agent"] !== computerId) {
+    if (req.headers["command"] === "reload")
+    {
+        LoadJson()
+        res.end("Complete!")
+
+        return
+    }
+
+    if (req.headers["user-agent"] !== computerId)
+    {
         console.log("Not Authenticated!")
         res.end("false")
+
         return
     }
     const username = req.headers["player"]
+    const door = req.headers["door"]
     const authenticationLevel = playerAuthentication[username] ?? 0
-    const doorLevel = doorAuthentication[req.headers["door"]] ?? 0
+    const doorLevel = doorAuthentication[door] ?? 0
 
     console.log(`Username: ${username}, AuthenticationLevel: ${authenticationLevel}, DoorLevel: ${doorLevel}`)
 
-    if (authenticationLevel >= doorLevel) {
-        res.end("true")
-
+    if (authenticationLevel >= doorLevel)
+    {
         console.log("Authenticated!")
+        res.end("true")
 
         return
     }
